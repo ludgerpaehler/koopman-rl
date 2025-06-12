be7fcde8c02901a642865e8fb130484c3d31ef98
Ludger Paehler <ludger.paehler@tum.de>
2025-06-12 10:00:08 +0000

    First full version of tuning script pre-debugging.

--- tuning/optuna.py
+++ tuning/optuna.py
@@ -1,7 +1,6 @@
 import os
 import runpy
 import sys
-import time
 
 import numpy as np
 
@@ -28,11 +27,11 @@ class HiddenPrints:
 
 
 def evaluate(config):
-    # Algorithm command
+    # Assemble the algorithm command
     algo_command = [f"--{key}={value}" for key, value in config.items()]
 
-    # Connect the runtime commands
-    sys.argv = algo_command + [f"--env-id={config.env_id}", f"--seed={config.seed}", "--track=False"]
+    # Connect the runtime commands & run it
+    sys.argv = algo_command + [f"--env-id={config["env-id"]}", f"--seed={config["seed"]}", "--track=False"]
     with HiddenPrints():
         _experiment = runpy.run_path(path_name=config["script"], run_name="__main__")
     return _experiment
@@ -49,36 +48,41 @@ def objective(config):
     ea = event_accumulator.EventAccumulator(f"runs/{experiment['run_name']}")
     ea.Reload()
     metric_values = [
-        scalar_event.value for scalar_event in ea.Scalars(self.metric)[-self.metric_last_n_average_window :]
+        scalar_event.value for scalar_event in ea.Scalars(config["metric"])[-config["metric-last-n-average-window"] :]
     ]
-    # TODO: For debugging
     print (
-        f"The average episodic return on {config["env_id"]} is {np.average(metric_values)} average over the last {config["metric_last_n_average_window"]} episodes."
+        f"The average episodic return on {config["env-id"]} is {np.average(metric_values)} average over the last {config["metric-last-n-average-window"]} episodes."
     )
-    if self.target_scores[env_id] is not None:
+    if config["target-score"] is not None:
         normalized_scores += [
-            (np.average(metric_values) - self.target_scores[env_id][0])
-            / (self.target_score[env_id][1] - self.target_scores[env_id][0])
+            (np.average(metric_values) - config["target_score"][0])
+            / (config["target_score"][1] - config["target_scores"][0])
         ]
     else:
         normalized_scores += [np.average(metric_values)]
     if run:
-        run.log({f"{env_id}_return": np.average(metric_values)})
+        run.log({f"{config["env-id"]}_return": np.average(metric_values)})
     aggregated_normalized_score = np.median(normalized_scores)
-    print(f"The median normalized score is {aggregated_normalized_score} with num_seeds={seed}")
-    tune.report({"iterations": seed, "aggregated_normalized_score": aggregated_normalized_score})  # TODO: This should be the step
+    print(f"The median normalized score is {aggregated_normalized_score} with num_seeds={config["seed"]}")
+    tune.report({"iterations": config["seed"], "aggregated_normalized_score": aggregated_normalized_score})
 
 
 # Definition of the search space
 search_space = {
+    "env-id": "CartPole-v1",
+    "seed": tune.randint(0, 10000),
+    "script": "cleanrl/ppo.py",
     "learning-rate": tune.loguniform(0.0003, 0.003),
     "num-minibatches": tune.choice([1, 2, 4]),
     "update-epochs": tune.choice([1, 2, 4, 8]),
     "num-steps": tune.choice([5, 16, 32, 64, 128]),
     "vf-coef": tune.uniform(0, 5),
     "max-grad-norm": tune.uniform(0, 5),
+    "target-score": [0, 500],
     "total-timesteps": 100000,
-    "num-envs": 16
+    "num-envs": 16,
+    "metric": "charts/episodic_return",
+    "metric-list-n-average-window": 50,
 }
 
 # Define the search algorithm
@@ -104,33 +108,4 @@ results = tuner.fit()
 # Print the best found hyperparameters of this initial trial
 print("Best hyperparameters found were: ", results.get_best_result().config)
 
-
-"""
-Providing an initial set of hyperparameters to seed the search
-"""
-
-# Set of initial parameters to seed the algorithm
-initial_params = [
-    {"width": 1, "height": 2, "activation": "relu"},
-    {"width": 4, "height": 2, "activation": "relu"},
-]
-
-# Seed the search algorithm with the initial parameters
-searcher = OptunaSearch(points_to_evaluate=initial_params)
-algo = ConcurrencyLimiter(searcher, max_concurrent=28)
-
-# Prime Tune and run the trial
-tuner = tune.Tuner(
-    objective,
-    tune_config=tune.TuneConfig(
-        metric="mean_loss",
-        mode="min",
-        search_alg=algo,
-        num_samples=num_samples,
-    ),
-    param_space=search_space,
-)
-results = tuner.fit()
-
-# Print the best results
-print("Best hyperparameters found were: ", results.get_best_result().config)
+# TODO: Just needs to be debugged now.
