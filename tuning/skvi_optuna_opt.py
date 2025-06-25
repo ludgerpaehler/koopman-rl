a17d92a2c3cb47c233467c6fd1c97ce4abb3288e
Ludger Paehler <ludger.paehler@tum.de>
2025-06-24 23:54:37 -0700

    First draft of hyperparameter optimization wrappers.

---
+++ tuning/skvi_optuna_opt.py
@@ -0,0 +1,97 @@
+import numpy as np
+import warnings
+
+import ray
+from ray import tune
+from ray.tune.search import ConcurrencyLimiter
+from ray.tune.search.optuna import OptunaSearch
+
+from opt_wrappers import skvi_tuning_wrapper
+
+
+def evaluate(config):
+    _experiment = skvi_tuning_wrapper(
+        seed=config["seed"],
+        env_id=config["env-id"],
+        learning_rate=config["learning-rate"],
+        number_of_actions=config["number-of-actions"],
+        number_of_training_epochs=config["num-epochs"],
+        batch_size=config["batch-size"],
+        number_of_paths=config["num-paths"],
+        number_of_steps_per_path=config["num-steps-per-path"],
+        state_order=config["state-order"],
+        action_order=config["action-order"],
+    )
+    return _experiment
+
+
+def objective(config):
+    _experiment = evaluate(config)
+
+    # Extract the metrics from the experiment
+    metric_values = [
+        scalar_event[0]
+        for scalar_event in _experiment[config["metric"]][
+            -config["metric-last-n-average-window"] :
+        ]
+    ]
+    if config["target-score"] is not None:
+        normalized_score = (np.average(metric_values) - config["target-score"][0]) / (
+            config["target-score"][1] - config["target-score"][0]
+        )
+    else:
+        normalized_score = np.average(metric_values)
+    tune.report(
+        {
+            "iterations": config["seed"],
+            "normalized_score": normalized_score,
+        }
+    )
+
+
+if __name__ == "__main__":
+    # Reduce the number of displayed error messages
+    warnings.filterwarnings("ignore")
+
+    # initialize Ray
+    ray.init(configure_logging=False)
+
+    # Definition of the search space
+    search_space = {
+        "env-id": "CartPole-v1",
+        "seed": tune.randint(0, 10000),
+        "learning-rate": tune.loguniform(0.0003, 0.003),
+        "number-of-actions": tune.choice([1, 2, 4]),
+        "number-of-train-epochs": tune.choice([1, 2, 4, 8]),
+        "batch-size": tune.choice([1, 2, 4, 8]),
+        "num-paths": tune.choice([1, 2, 4, 8]),
+        "num-steps-per-path": tune.choice([1, 2, 4, 8]),
+        "state-order": tune.choice([1, 2, 4, 8]),
+        "action-order": tune.choice([1, 2, 4, 8]),
+        "target-score": [0, 500],
+        "total-timesteps": 100000,
+        "num-envs": 16,
+        "metric": "charts/episodic_return",
+        "metric-last-n-average-window": 50,
+    }
+
+    # Initialize the search algorithm
+    algo = OptunaSearch()
+    algo = ConcurrencyLimiter(algo, max_concurrent=4)
+    num_samples = 50
+
+    # Define the Tune trial & run it
+    tuner = tune.Tuner(
+        objective,
+        tune_config=tune.TuneConfig(
+            metric="normalized_score",
+            mode="max",
+            search_alg=algo,
+            num_samples=num_samples,
+        ),
+        param_space=search_space,
+    )
+    results = tuner.fit()
+
+    # Print the best found hyperparameters of this initial trial
+    print("Best hyperparameters found were: ", results.get_best_result().config)
